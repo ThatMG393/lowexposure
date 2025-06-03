40352dfd1d3cf00f9811d05f23ca13300f0ed64c
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2025-06-03 00:19:08 +0000

    heee

--- src/main/java/com/thatmg393/lowexposure/LowExposure.java
+++ src/main/java/com/thatmg393/lowexposure/LowExposure.java
@@ -1,13 +1,40 @@
 package com.thatmg393.lowexposure;
 
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+
 @Mod(LowExposure.MOD_ID)
-@EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)   
+@EventBusSubscriber(
+    modid = LowExposure.MOD_ID,
+    bus = EventBusSubscriber.Bus.MOD,
+    value = Dist.CLIENT
+)   
 public class LowExposure {
     public static final String MOD_ID = "lowexposure";
     private static final Logger LOGGER = LogUtils.getLogger();
     
     public LowExposure(IEventBus modEventBus, ModContainer modContainer) {
+        if (!ModList.get().isLoaded("veil")) {
+            LOGGER.error("Veil is NOT installed!");
+            LOGGER.error("Veil is required to see the post-processinf effect!");
+        }
+
         LOGGER.info("Here I am! (null)");
+
+        if (!ModList.get().isLoaded("thebrokenscript")) {
+            LOGGER.info("Best used with The Broken Script!");
+        }
     }
 
     @SubscribeEvent
