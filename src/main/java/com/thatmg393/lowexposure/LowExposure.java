62f15391743aed7b4d49fcf0d337a1b4aa3bdea7
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2025-06-03 02:52:44 +0000

    e

--- src/main/java/com/thatmg393/lowexposure/LowExposure.java
+++ src/main/java/com/thatmg393/lowexposure/LowExposure.java
@@ -4,7 +4,10 @@ import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
+import foundry.veil.api.client.render.VeilRenderSystem;
+import foundry.veil.platform.VeilEventPlatform;
 import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -14,7 +17,10 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
-@Mod(LowExposure.MOD_ID)
+@Mod(
+    value=LowExposure.MOD_ID,
+    dist={ Dist.CLIENT }
+)
 @EventBusSubscriber(
     modid = LowExposure.MOD_ID,
     bus = EventBusSubscriber.Bus.MOD,
@@ -22,24 +28,39 @@ import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 )   
 public class LowExposure {
     public static final String MOD_ID = "lowexposure";
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public static ResourceLocation LOW_EXPOSURE_PIPELINE = ResourceLocation.fromNamespaceAndPath(MOD_ID, "lowexpo");
     
-    public LowExposure(IEventBus modEventBus, ModContainer modContainer) {
-        if (!ModList.get().isLoaded("veil")) {
-            LOGGER.error("Veil is NOT installed!");
-            LOGGER.error("Veil is required to see the post-processinf effect!");
-        }
-
-        LOGGER.info("Here I am! (null)");
-
-        if (!ModList.get().isLoaded("thebrokenscript")) {
-            LOGGER.info("Best used with The Broken Script!");
-        }
-    }
+    private LowExposure() { }
 
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
-        LOGGER.info("HELLO FROM CLIENT SETUP");
-        LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        LOGGER.info("Low Exposure : loaded!");
+        LOGGER.info("Here I am! (null)");
+
+        if (!ModList.get().isLoaded("veil")) {
+            LOGGER.error("Veil is NOT installed!");
+            LOGGER.error("Veil is required to see the post-processing effect!");
+
+            return;
+        }
+            
+        VeilEventPlatform.INSTANCE.onVeilRenderLevelStage((
+            stage, levelRenderer, bufferSource,
+            matrixStack, matrix4fc, matrix4fc2,
+            n, deltaTracker, camera, frustum
+        ) -> {
+        if (VeilRenderSystem.renderer().getPostProcessingManager().isActive(LOW_EXPOSURE_PIPELINE)) return;
+            VeilRenderSystem.renderer().getPostProcessingManager().add(LOW_EXPOSURE_PIPELINE);
+        });
+            
+        LOGGER.info("Successfully registered post-processing shader!");
+            
+        // TODO: customizable color mix and contrast through uniforms
+
+        if (!ModList.get().isLoaded("thebrokenscript")) {
+            LOGGER.info("This mod is best used with The Broken Script!");
+        }
     }
 }
